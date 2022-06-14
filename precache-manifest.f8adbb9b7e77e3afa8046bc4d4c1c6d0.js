de09456e5ca95d407ab2a4d863375be76d3a1b6b
dato <dato.tchkadua.1@iliauni.edu.ge>
2022-06-14 19:15:57 +0400

    Updates

---
+++ precache-manifest.f8adbb9b7e77e3afa8046bc4d4c1c6d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e787c7b357732bb521b963906a4a216",
+    "url": "/phones/index.html"
+  },
+  {
+    "revision": "a86edc13480ebeee9475",
+    "url": "/phones/static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/phones/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/phones/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86edc13480ebeee9475",
+    "url": "/phones/static/js/main.a069190c.chunk.js"
+  },
+  {
+    "revision": "e161eda6703210572a84",
+    "url": "/phones/static/js/runtime-main.4342b5df.js"
+  }
+]);
